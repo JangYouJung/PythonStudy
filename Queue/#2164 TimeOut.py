ffc0bb1a95ea34a535ecfa37f502eb7a6ec33da3
YueJeong <80906691+YueJeong@users.noreply.github.com>
2022-06-23 23:57:19 +0900

    Modified #2164 TimeOut

--- Queue/#2164 TimeOut.py
+++ Queue/#2164 TimeOut.py
@@ -4,12 +4,12 @@ cards = []
 for i in range(1,n+1):
     cards.append(i)
     
-while(1):   
+while(1):       
+    cards.pop(0)
     if(len(cards) == 1):
         print(cards[0])
         break
     
-    cards.pop(0)
     cards.append(cards[0])
     cards.pop(0)
     
