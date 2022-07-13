9dd1343a52250376494a0d4aa08162c5ea4c2d42
You Jeong Jang <80906691+YueJeong@users.noreply.github.com>
2022-07-13 23:53:03 +0900

    Create #2438_ERROr.py

---
+++ 반복문/#2438_ERROr.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 13 23:50:33 2022
+
+@author: sunny
+"""
+
+n=int(input())
+for i in range(1,n+1):
+    for j in range(i):
+        print("*")
+    print("\n")
